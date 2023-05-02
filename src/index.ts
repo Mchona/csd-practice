76d5ae9f82611cd6dbbb91f1a8c7f1fa81595736
Mchona <128210583+Mchona@users.noreply.github.com>
2023-05-02 19:20:18 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+class Department {
+  name: string;
+  employees: string[] = [];
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department :" + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+
+// accountingCopy.describe();
